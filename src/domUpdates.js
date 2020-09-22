383554c17b88790ebb1a71e1c1155732db871ce0
Eric Berglund <ericberglund117@gmail.com>
2020-09-22 16:26:21 -0600

    Add displayTravelerTrips functionality

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,9 +1,53 @@
-const domUpdates {
+import querySelectorNodes from "./index";
 
-  renderAmountSpent: (traveler, tripData, destinationData) => {
+let domUpdates = {
+
+  toggleViews() {
+    signInView.classList.toggle('hidden');
+    travelerSection.classList.toggle('hidden');
+  },
+
+  welcomeTraveler(traveler) {
+    let firstName = currentTraveler.name.split(" ")[0];
+    let welcomeMsg = `
+      <div class="welcome-msg">
+        <h1>Welcome ${firstName}!</h1>
+      </div>`;
+    document.querySelector(".travel-tracker-header").insertAdjacentHTML("afterbegin",
+    welcomeMsg)
+  },
+
+  displayTravelerTrips(traveler, destinations) {
+    querySelectorNodes.bookedTripsCardsSection.innerHTML = '';
+    traveler.trips.forEach(trip => {
+      querySelectorNodes.bookedTripsCardsSection.insertAdjacentHTML(`beforeend`,
+        `<article class="trips-card-body" id="booked-trips-section"></article>
+          <p>Destination: ${destinations[trip.destinationID - 1].destination}</p>
+          <p>Travelers: ${trip.travelers}</p>
+          <p>Date: ${trip.date}</p>
+          <p>Status: ${trip.status}</p>
+          <hr>
+        </article>`
+      );
+    });
+  },
+
+  displayAmountSpent(traveler, tripData, destinations) {
    let amountSpent = traveler.getAmountSpent(tripData, destinationData);
    yearlyAmountSpent.innerHtml(`<h2>You spent $${amountSpent} on traveling this year!</h2>`);
  },
+
+ displayDestinationCards(travler) {
+   traveler.allDestinations.forEach(destination => {
+     destinationsCards.insertAdjacentHTML('beforeend',
+      `<article class="sub-sub-card" id="desinations-sub-card">
+            <h3>${destination.destination}</h3>
+            <input type="image" class="destination-image" id="${destination.id}" src="${destination.image}" alt="${destination.alt}"/>
+          </article>`
+        )
+   })
+ }
+
 }
 
 export default domUpdates;
